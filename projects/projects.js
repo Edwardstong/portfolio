e7918fc996aa171b07cb82f5751c37434cee47cb
Edwardstong <edtong@ucsd.edu>
2025-10-24 17:33:13 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,9 @@
 import { fetchJSON, renderProjects } from '../global.js';
+
 const projects = await fetchJSON('../lib/projects.json');
+
+const title = document.querySelector('.projects-title');
+if (title) title.textContent = `Projects (${Array.isArray(projects) ? projects.length : 0})`;
+
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
